943e969032e5e2466520b93e76c4584ca76f1252
Andres Melendez <32958027+andresceleron@users.noreply.github.com>
2018-04-11 22:57:17 +0100

    Add files via upload

---
+++ php/phpsqlsearch_dbinfo.php
@@ -0,0 +1,35 @@
+
+<?php
+$username="root";
+$password="";
+$database="dbcraic";
+$servername = "localhost";
+
+/*		Server AWS connection		
+		$username = "dbcraicAdmin";
+		$password = "passw1234..."; check with Tong!
+		$database = "dbcraic";
+		$servername = "dbcraicinstance.cdxdfgohwdom.eu-west-1.rds.amazonaws.com";
+
+		AWS web server: 
+		host: 54.194.81.61 
+		username: root
+		password: pass...!
+		port: 22
+		
+img route: ./img/filename.....	
+	jakepres@amazon.comdniwood@amazon.com
+  cognito for security and autentications ...
+scrypt o pbkdf2 scrits for encrypt passwords...   
+
+bootstrap, angular and ionic like framwork7... 
+
+
+*/
+
+
+
+?>
+
+
+
